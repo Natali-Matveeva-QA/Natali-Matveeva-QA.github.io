483e98acbb34f6463778a7dc6f24ba2113bb480b
Natali <mk7407863@gmail.com>
2024-11-19 19:30:22 +0300

    Update js.js

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('QA Enhineer много не надо, ему нужно всё')
+	alert('QA Engineer много не надо, ему нужно всё')
 })
